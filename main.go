49d32253bd733d4779f5602352b76f689ee6fb55
Scott Cunningham <scottcunningham92@gmail.com>
2022-01-13 17:48:55 +0000

    Support dumping cpu profiles

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/jessevdk/go-flags"
+	"os"
+	"runtime/pprof"
 )
 
 type Args struct {
-	ROM         string `short:"r" long:"rom" description:"Path to chip-8 ROM to execute" required:"yes"`
-	RefreshRate int    `long:"refresh-rate" description:"Cycles per second" default:"60"`
-	DebugMode   bool   `short:"d" long:"debug" description:"Print verbose data about instructions"`
+	ROM         string  `short:"r" long:"rom" description:"Path to chip-8 ROM to execute" required:"yes"`
+	RefreshRate int     `long:"refresh-rate" description:"Cycles per second" default:"60"`
+	DebugMode   bool    `short:"d" long:"debug" description:"Print verbose data about instructions"`
+	Profile     *string `long:"profile" description:"Dump a CPU profile into the given file"`
 }
 
 func main() {
@@ -15,6 +18,15 @@ func main() {
 	_, err := flags.Parse(&args)
 	if err != nil {
 		panic(err)
+	}
+
+	if args.Profile != nil {
+		f, err := os.Create(args.Profile)
+		if err != nil {
+			panic(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
 	}
 
 	chip := NewChip8()
